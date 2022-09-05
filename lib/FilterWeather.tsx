151e812e6756cda3824660464d52faac2e0871dd
Phil Stene <phil.stene@gmail.com>
2022-09-05 00:31:05 -0400

    cleaned up FilterWeather

--- lib/FilterWeather.tsx
+++ lib/FilterWeather.tsx
@@ -3,18 +3,25 @@ import { FilteredWeather, Weather, WeatherDataMode } from "../types/types";
 export function FilterWeather(data: Weather, mode: WeatherDataMode): FilteredWeather {
   const exclusions: string[] = [];
 
-  if (mode === WeatherDataMode.BEACH) {
-    if (data.temperature < 70) exclusions.push("Too Cold");
+  switch (mode) {
+    case WeatherDataMode.BEACH:
+      if (data.temperature < 70) exclusions.push("Too Cold");
 
-    if (data.windSpeed > 20) exclusions.push("Too Windy");
+      if (data.windSpeed > 20) exclusions.push("Too Windy");
 
-    if (data.clouds > 15) exclusions.push("Too Cloudy");
+      if (data.clouds > 15) exclusions.push("Too Cloudy");
 
-    if (data.alert) exclusions.push("A weather alert for the area is in effect");
-  } else {
-    if (data.temperature > 50) exclusions.push("Too Warm");
+      if (data.alert) exclusions.push("A weather alert for the area is in effect");
+      break;
+    case WeatherDataMode.SKI:
+      if (data.temperature > 50) exclusions.push("Too Warm");
 
-    if (data.alert) exclusions.push("A weather alert for the area is in effect");
+      if (data.alert) exclusions.push("A weather alert for the area is in effect");
+      break;
+    case WeatherDataMode.NEITHER:
+      break;
+    default:
+      break;
   }
 
   const newFilteredWeather: FilteredWeather = { ...data, exclusions: exclusions };
