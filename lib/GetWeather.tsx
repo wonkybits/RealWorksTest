ac77cd4ce4dba85c5a46bc3f48b54c8efa236904
Phil Stene <phil.stene@gmail.com>
2022-09-05 00:51:20 -0400

    added error catching to GetWeather

--- lib/GetWeather.tsx
+++ lib/GetWeather.tsx
@@ -1,22 +1,28 @@
 import { Weather, City, FilteredWeather, WeatherDataMode } from "../types/types";
 import { FilterWeather } from "./FilterWeather";
 
-export async function GetWeather(data: City, mode: WeatherDataMode): Promise<FilteredWeather> {
-  const localizedWeather = await fetch(
-    `https://api.openweathermap.org/data/2.5/onecall?lat=${data.lat}&lon=${data.lng}&exclude=minutely,hourly,daily,alerts&appid=${process.env.OPENWEATHERKEY}&units=imperial`
-  );
-  const jsonWeather = await localizedWeather.json();
-  const cityWeather: Weather = {
-    name: data.name,
-    lat: data.lat,
-    lng: data.lng,
-    temperature: jsonWeather.current.temp,
-    windSpeed: jsonWeather.current.wind_speed,
-    clouds: jsonWeather.current.clouds,
-    alert: jsonWeather.current.alerts ? jsonWeather.current.alerts[0].event : null,
-  };
+export async function GetWeather(data: City, mode: WeatherDataMode): Promise<FilteredWeather | null> {
+  try {
+    const localizedWeather = await fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${data.lat}&lon=${data.lng}&exclude=minutely,hourly,daily,alerts&appid=${process.env.OPENWEATHERKEY}&units=imperial`
+    );
+    const jsonWeather = await localizedWeather.json();
+    const cityWeather: Weather = {
+      name: data.name,
+      lat: data.lat,
+      lng: data.lng,
+      temperature: jsonWeather.current.temp,
+      windSpeed: jsonWeather.current.wind_speed,
+      clouds: jsonWeather.current.clouds,
+      alert: jsonWeather.current.alerts ? jsonWeather.current.alerts[0].event : null,
+    };
 
-  const filteredWeather = FilterWeather(cityWeather, mode);
+    const filteredWeather = FilterWeather(cityWeather, mode);
 
-  return filteredWeather;
+    return filteredWeather;
+  } catch (error) {
+    console.log(`An error occured while loading weather for City: ${data.name}, error -> ${error}`);
+  }
+
+  return null;
 }
